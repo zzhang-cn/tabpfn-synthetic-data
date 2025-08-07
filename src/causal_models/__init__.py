df3d103058939893b21aaeaee633d9c3e793c8ca
Zheng Zhang <zzhang@gmail.com>
2025-08-07 15:57:13 +0800

    Create __init__.py

---
+++ src/causal_models/__init__.py
@@ -0,0 +1,6 @@
+"""Causal model and graph generation modules"""
+
+from .graph_generator import CausalGraphGenerator
+from .scm import StructuralCausalModel
+
+__all__ = ["CausalGraphGenerator", "StructuralCausalModel"]
